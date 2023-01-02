cd70d21283f37929f7acd0c98f4aecb5633b9739
Tetiana Mandziuk <tetianaman@wix.com>
2023-01-02 13:02:53 +0100

    dfvsdf

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
-                >Learn React 1</a>
+                >Learn React 12</a>
             </header>
         </div>
     );
